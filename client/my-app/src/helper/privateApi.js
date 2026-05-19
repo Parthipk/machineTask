f627b55b41045426d02dc303411a7eaa8d75632b
Parthip.k <parthipkanish@gmail.com>
2026-05-20 00:29:02 +0530

    feature : api changing

--- client/my-app/src/helper/privateApi.js
+++ client/my-app/src/helper/privateApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const privateApi = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://localhost:3000/api/v1/user",
 });
 
 privateApi.interceptors.request.use(
--- client/my-app/src/helper/publicApi.js
+++ client/my-app/src/helper/publicApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const publicApi = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://localhost:3000/api/v1/user",
 });
 
 export default publicApi;
